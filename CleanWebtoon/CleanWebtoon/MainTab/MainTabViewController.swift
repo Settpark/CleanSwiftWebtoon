a930ef62a2cb08dd9c4254abe12d94ad4270259a
settpark <park9368@naver.com>
2023-06-21 13:32:51 +0900

    init: 프로젝트 초기화
    Clean Swift 예제 Template 생성

---
+++ CleanWebtoon/CleanWebtoon/MainTab/MainTabViewController.swift
@@ -0,0 +1,8 @@
+//
+//  MainTabViewController.swift
+//  CleanWebtoon
+//
+//  Created by temp_name on 2023/06/21.
+//
+
+import Foundation
---
+++ CleanWebtoon/CleanWebtoon/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CleanWebtoon
+//
+//  Created by temp_name on 2023/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+    func setupViews() {
+        view.backgroundColor = .white
+    }
+}
+
